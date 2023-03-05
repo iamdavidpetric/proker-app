ca4bc2d36a3eb3ef0222e2288d77529ab3e5104a
David Petric <94174624+iamdavidpetric@users.noreply.github.com>
2023-03-05 21:04:14 +0200

    PA-4 - Create Api service [v0.5.0] (#5)

---
+++ src/logic/api/index.js
@@ -0,0 +1,75 @@
+import axios from 'axios';
+
+const defaultConfig = {
+  baseUrl: `${process.env.REACT_APP_API_URL}${process.env.REACT_APP_API_PATH_PREFIX}`,
+  timeout: parseInt(process.env.REACT_APP_TIMEOUT),
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+};
+
+const api = axios.create(defaultConfig);
+
+const ownInterceptors = {
+  handleRequestError: error => {
+    return Promise.reject(error);
+  },
+  handleResponseError: error => {
+    return Promise.reject(error);
+  },
+  handleResponse: config => {
+    return config;
+  },
+  handleRequest: config => {
+    return config;
+  },
+};
+
+api.interceptors.request.use(ownInterceptors.handleRequest, ownInterceptors.handleRequestError);
+
+api.interceptors.response.use(ownInterceptors.handleResponse, ownInterceptors.handleResponseError);
+
+const Api = {
+  API_CALL: 'API_CALL',
+
+  catchError: error => {
+    return error.response.data;
+  },
+
+  delete: (path, config = defaultConfig) => {
+    const request = api.delete(path, config);
+    request.catch(Api.catchError);
+    return request;
+  },
+
+  get: (path, params = {}, config = defaultConfig) => {
+    const request = api.get(path, { ...config, params });
+    request.catch(Api.catchError);
+    return request;
+  },
+
+  patch: (path, body, config = defaultConfig) => {
+    const request = api.patch(path, body, config);
+    request.catch(Api.catchError);
+    return request;
+  },
+
+  post: (path, body, config = defaultConfig) => {
+    const request = api.post(path, body, config);
+    request.catch(Api.catchError);
+    return request;
+  },
+
+  put: (path, body, config = defaultConfig) => {
+    const request = api.put(path, body, config);
+    request.catch(Api.catchError);
+    return request;
+  },
+
+  setHeaders: headers => {
+    api.defaults.headers.common = headers;
+  },
+};
+
+export default Api;
